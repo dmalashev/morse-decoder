eb159cdc2b9b194854d6bf25207726da6b74a838
Dmitry Malashev <dmalashev@gmail.com>
2024-10-16 02:56:54 +0300

    feat: solve the morse code decoder task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let letters = [];
+    let residualExpression = expr;
+    let result = '';
 
+    while (residualExpression !== '') {
+        letters.push(residualExpression.slice(0, 10));
+        residualExpression = residualExpression.slice(10);
+    }
+
+    for (let letter of letters) {
+        if (letter === '**********') {
+            result += ' ';
+        } else {
+            let residualLetter = letter;
+            let morseLetter = '';
+
+            while (residualLetter !== '') {
+                if (residualLetter.slice(0, 2) === '10') {
+                    morseLetter += '.';
+                } else if (residualLetter.slice(0, 2) === '11') {
+                    morseLetter += '-';
+                } else if (residualLetter.slice(0, 2) === '00') {
+                    morseLetter += '';
+                }
+
+                residualLetter = residualLetter.slice(2);
+            }
+
+            result += MORSE_TABLE[morseLetter];
+        }
+    }
+    return result;
+}
+     
 module.exports = {
     decode
 }
